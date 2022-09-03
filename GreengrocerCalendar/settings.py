cc7747f2926878921f43cac06535bae0d4c16e4b
Dearbhla Hughes <dearbhla@dearbh.la>
2022-09-03 19:06:44 +0100

    Handling secrets

--- GreengrocerCalendar/settings.py
+++ GreengrocerCalendar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tn2)aal^+*a&o4!*u7_u#466_oy4k5j(!#@^rkcp$8(10bn&#0'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
